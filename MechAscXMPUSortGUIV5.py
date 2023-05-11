dee6ad92b645c4897139033af8ef7ea9633da9e6
FuriousGremlin <76954313+FuriousGremlin@users.noreply.github.com>
2023-05-11 21:02:38 +0200

    MechAscX Item Manager v5.0.1
    Added comments ito the tkinter section

--- MechAscXMPUSortGUIV5.py
+++ MechAscXMPUSortGUIV5.py
@@ -77,6 +77,7 @@ def sort_items():
         else:
             items = [item for item in items if item.item_type == item_type_filter]
 
+    # Apply MPU/Multiplier sort
     if sort_key == "MPU":
         items.sort(key=lambda x: x.mpu, reverse=True)
     elif sort_key == "Multiplier":
@@ -127,89 +128,106 @@ root.title("Mechanical Ascension X Item Manager")
 form_frame = ttk.Frame(root, padding="10")
 form_frame.grid(row=0, column=0, sticky=(tk.W, tk.E))
 
+# Input item name
 name_label = ttk.Label(form_frame, text="Name:")
 name_label.grid(row=0, column=0, sticky=tk.W)
 name_entry = ttk.Entry(form_frame)
 name_entry.grid(row=0, column=1, sticky=tk.W)
 
+# Select if the item has a max value
 max_value_label = ttk.Label(form_frame, text="Max Value:")
 max_value_label.grid(row=0, column=2, sticky=tk.W)
 max_value_var = tk.BooleanVar()
 max_value_check = ttk.Checkbutton(form_frame, variable=max_value_var, command=update_item_name)
 max_value_check.grid(row=0, column=3, sticky=tk.W)
 
+# Input item length
 length_label = ttk.Label(form_frame, text="Length:")
 length_label.grid(row=1, column=0, sticky=tk.W)
 length_entry = ttk.Entry(form_frame)
 length_entry.grid(row=1, column=1, sticky=tk.W)
 
+# Input item width
 width_label = ttk.Label(form_frame, text="Width:")
 width_label.grid(row=2, column=0, sticky=tk.W)
 width_entry = ttk.Entry(form_frame)
 width_entry.grid(row=2, column=1, sticky=tk.W)
 
+# Input item multiplier
 multiplier_label = ttk.Label(form_frame, text="Multiplier:")
 multiplier_label.grid(row=3, column=0, sticky=tk.W)
 multiplier_entry = ttk.Entry(form_frame)
 multiplier_entry.grid(row=3, column=1, sticky=tk.W)
 
+# Input item status effects
 status_effect_label = ttk.Label(form_frame, text="Status Effect:")
 status_effect_label.grid(row=4, column=0, sticky=tk.W)
 status_effect_entry = ttk.Entry(form_frame)
 status_effect_entry.grid(row=4, column=1, sticky=tk.W)
 
+# Input what item is buffed by
 buffed_by_label = ttk.Label(form_frame, text="Buffed by:")
 buffed_by_label.grid(row=6, column=0, sticky=tk.W)
 buffed_by_entry = ttk.Entry(form_frame)
 buffed_by_entry.grid(row=6, column=1, sticky=tk.W)
 
+# Select if item can cleanse
 can_cleanse_label = ttk.Label(form_frame, text="Can Cleanse:")
 can_cleanse_label.grid(row=7, column=0, sticky=tk.W)
 can_cleanse_var = tk.BooleanVar()
 can_cleanse_check = ttk.Checkbutton(form_frame, variable=can_cleanse_var, command=toggle_cleansable_effects)
 can_cleanse_check.grid(row=7, column=1, sticky=tk.W)
 
+# Input cleansable effects
 cleansable_effects_label = ttk.Label(form_frame, text="Cleansable Effects:")
 cleansable_effects_entry = ttk.Entry(form_frame)
 
+# Enter item use limit
 use_limit_label = ttk.Label(form_frame, text="Use Limit:")
 use_limit_label.grid(row=9, column=0, sticky=tk.W)
 use_limit_entry = ttk.Entry(form_frame)
 use_limit_entry.grid(row=9, column=1, sticky=tk.W)
 
+# Select if item has unlimited uses
 unlimited_uses_label = ttk.Label(form_frame, text="Unlimited Uses:")
 unlimited_uses_label.grid(row=10, column=0, sticky=tk.W)
 unlimited_uses_var = tk.BooleanVar()
 unlimited_uses_check = ttk.Checkbutton(form_frame, variable=unlimited_uses_var, command=toggle_diminishing_rate)
 unlimited_uses_check.grid(row=10, column=1, sticky=tk.W)
 
+# Enter diminishing rate
 diminishing_rate_label = ttk.Label(form_frame, text="Diminishing Rate:")
 diminishing_rate_entry = ttk.Entry(form_frame)
 
+# Select item conveyor height
 conveyor_height_label = ttk.Label(form_frame, text="Conveyor Height:")
 conveyor_height_label.grid(row=12, column=0, sticky=tk.W)
 conveyor_height_combobox = ttk.Combobox(form_frame, values=["Low", "Mid", "High"], state="readonly")
 conveyor_height_combobox.grid(row=12, column=1, sticky=tk.W)
 conveyor_height_combobox.current(0)
 
+# Select item type
 item_type_label = ttk.Label(form_frame, text="Item Type:")
 item_type_label.grid(row=13, column=0, sticky=tk.W)
 item_type_combobox = ttk.Combobox(form_frame, values=["Mine", "Processor", "Upgrader", "Portable", "Static"], state="readonly")
 item_type_combobox.grid(row=13, column=1, sticky=tk.W)
 item_type_combobox.current(0)
 
+# Select if item is soulbound
 soulbound_label = ttk.Label(form_frame, text="Soulbound:")
 soulbound_label.grid(row=14, column=0, sticky=tk.W)
 soulbound_var = tk.BooleanVar()
 soulbound_check = ttk.Checkbutton(form_frame, variable=soulbound_var)
 soulbound_check.grid(row=14, column=1, sticky=tk.W)
 
+# Select if item is escrow proof
 escrow_proof_label = ttk.Label(form_frame, text="Escrow proof:")
 escrow_proof_label.grid(row=14, column=2, sticky=tk.W)
 escrow_proof_var = tk.BooleanVar()
 escrow_proof_check = ttk.Checkbutton(form_frame, variable=escrow_proof_var)
 escrow_proof_check.grid(row=14, column=3, sticky=tk.W)
 
+# Add item button
 add_button = ttk.Button(form_frame, text="Add Item", command=add_item)
 add_button.grid(row=15, columnspan=2, sticky=(tk.W, tk.E))
 
@@ -225,12 +243,14 @@ sort_mpu.grid(row=0, column=1, sticky=tk.W)
 sort_multiplier = ttk.Radiobutton(sort_frame, text="Multiplier", variable=sort_var, value="Multiplier")
 sort_multiplier.grid(row=0, column=2, sticky=tk.W)
 
+# Select item type filter
 item_type_filter_label = ttk.Label(sort_frame, text="Item Type Filter:")
 item_type_filter_label.grid(row=1, column=0, sticky=tk.W)
 item_type_filter_combobox = ttk.Combobox(sort_frame, values=["All", "Soulbound", "Escrow proof", "Mine", "Processor", "Upgrader", "Portable", "Static"], state="readonly")
 item_type_filter_combobox.grid(row=1, column=1, sticky=tk.W)
 item_type_filter_combobox.current(0)
 
+# Sort button
 sort_button = ttk.Button(sort_frame, text="Sort Items", command=sort_items)
 sort_button.grid(row=0, column=3, sticky=(tk.W, tk.E))
 
